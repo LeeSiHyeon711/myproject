96c171a5bc2b12bc92fda76327942684c5464948
Si Hyeon Lee <dltlgus1230@naver.com>
2025-10-02 19:29:57 +0900

    [feat] paginate

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -9,10 +9,11 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
-    question_list = Question.query.order_by(Question.create_date.desc())
+    page = request.args.get('page', type=int, default=1)
+    question_list = Question.query.order_by(Question.create_date.desc()).paginate(page=page, per_page=10, error_out=False)
     return render_template('question/question_list.html', question_list=question_list)
 
-@bp.route('detail/<int:question_id>/')
+@bp.route('/detail/<int:question_id>/')
 def detail(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
